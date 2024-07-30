235545a442acbc7b31ea1594d9dba54d4e7b2572
Lukas Rosenfeldt <lukasrosenfeldt@gmail.com>
2024-07-30 15:27:57 +0200

    chore: add example for commerce case creation with auto execute order

--- examples/demo-app/src/Examples/CommerceCaseApiExample.php
+++ examples/demo-app/src/Examples/CommerceCaseApiExample.php
@@ -4,12 +4,26 @@ namespace DemoApp\Examples;
 
 use PayoneCommercePlatform\Sdk\ApiClient\CommerceCaseApiClient;
 use PayoneCommercePlatform\Sdk\CommunicatorConfiguration;
+use PayoneCommercePlatform\Sdk\Errors\ApiErrorResponseException;
 use PayoneCommercePlatform\Sdk\Models\Address;
+use PayoneCommercePlatform\Sdk\Models\AmountOfMoney;
+use PayoneCommercePlatform\Sdk\Models\BankAccountInformation;
+use PayoneCommercePlatform\Sdk\Models\CartItemInput;
+use PayoneCommercePlatform\Sdk\Models\CartItemInvoiceData;
 use PayoneCommercePlatform\Sdk\Models\CompanyInformation;
+use PayoneCommercePlatform\Sdk\Models\CreateCheckoutRequest;
 use PayoneCommercePlatform\Sdk\Models\CreateCommerceCaseRequest;
 use PayoneCommercePlatform\Sdk\Models\Customer;
+use PayoneCommercePlatform\Sdk\Models\FinancingPaymentMethodSpecificInput;
+use PayoneCommercePlatform\Sdk\Models\OrderItem;
+use PayoneCommercePlatform\Sdk\Models\OrderRequest;
+use PayoneCommercePlatform\Sdk\Models\OrderType;
+use PayoneCommercePlatform\Sdk\Models\PaymentMethodSpecificInput;
+use PayoneCommercePlatform\Sdk\Models\PaymentProduct3392SpecificInput;
 use PayoneCommercePlatform\Sdk\Models\PersonalInformation;
 use PayoneCommercePlatform\Sdk\Models\PersonalName;
+use PayoneCommercePlatform\Sdk\Models\References;
+use PayoneCommercePlatform\Sdk\Models\ShoppingCartInput;
 
 class CommerceCaseApiExample
 {
@@ -82,5 +96,40 @@ class CommerceCaseApiExample
 
         $response = $this->client->getCommerceCase($this->merchantId, $this->getCommerceCaseId());
         var_dump($response);
+    }
+
+    public function runAndComplete(): void
+    {
+        $createCheckoutRequest = new CreateCheckoutRequest(
+            amountOfMoney: new AmountOfMoney(1600, 'EUR'),
+            orderRequest: new OrderRequest(
+                orderReferences: new References('ref'),
+                orderType: OrderType::FULL,
+                paymentMethodSpecificInput: new PaymentMethodSpecificInput(
+                    financingPaymentMethodSpecificInput: new FinancingPaymentMethodSpecificInput(
+                        paymentProductId: 3392,
+                        requiresApproval: false,
+                        paymentProduct3392SpecificInput: new PaymentProduct3392SpecificInput(
+                            new BankAccountInformation(
+                                iban: 'DE05500105178431848295',
+                                accountHolder: 'Robert Jordan',
+                            ),
+                        ),
+                    )
+                ),
+            ),
+            shoppingCart: new ShoppingCartInput(
+                items: [new CartItemInput(invoiceData: new CartItemInvoiceData('shiny thing'))]
+            ),
+            autoExecuteOrder: true
+        );
+        $request = new CreateCommerceCaseRequest(checkout: $createCheckoutRequest);
+
+        try {
+            $response = $this->client->createCommerceCase($this->merchantId, $request);
+            var_dump($response);
+        } catch (ApiErrorResponseException $e) {
+            var_dump($e->getErrors());
+        }
     }
 }
