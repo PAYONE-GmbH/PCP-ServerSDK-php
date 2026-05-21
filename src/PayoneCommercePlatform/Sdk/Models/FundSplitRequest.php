7cefca5141290185a209c9a5c0022efd5474a1c6
Dirk Ehrhardt <d.ehrhardt@nanogiants.de>
2026-05-21 11:58:50 +0200

    feat: update API version to 1.55.0

---
+++ src/PayoneCommercePlatform/Sdk/Models/FundSplitRequest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace PayoneCommercePlatform\Sdk\Models;
+
+use Symfony\Component\Serializer\Annotation\SerializedName;
+
+class FundSplitRequest
+{
+    #[SerializedName('fundSplit')]
+    protected FundSplit $fundSplit;
+
+    public function __construct(FundSplit $fundSplit)
+    {
+        $this->fundSplit = $fundSplit;
+    }
+
+    public function getFundSplit(): FundSplit
+    {
+        return $this->fundSplit;
+    }
+
+    public function setFundSplit(FundSplit $fundSplit): self
+    {
+        $this->fundSplit = $fundSplit;
+        return $this;
+    }
+}
